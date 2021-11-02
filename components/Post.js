e9b3f405d0e0697e8e2bc506ce7fe9ac13c62a3e
minu <gkqkehs10@naver.com>
2021-11-02 10:52:06 +0900

    just frontend

---
+++ components/Post.js
@@ -0,0 +1,62 @@
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon,
+    ChatIcon,
+    DotsHorizontalIcon,
+    BookmarkIcon,
+    EmojiHappyIcon,
+} from "@heroicons/react/outline"
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid' 
+
+function Post() {
+    return (
+        <div className="bg-white my-7 border rounded-sm:">
+            {/* Header */}
+            <div className="flex items-center p-5">
+                <img src="https://links.papareact.com/3ke" className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+                    alt="" />
+                <p className="flex-1 font-bold">UserName</p>
+                <DotsHorizontalIcon className="h-5" /> 
+                </div>
+
+            {/* img */}
+            <img src="https://links.papareact.com/3ke" className='object-cover w-full' alt=""/>
+
+            {/* Buttons */}
+            <div className='flex justify-between px-4 pt-4'>
+                <div className='flex space-x-4'>
+                    <HeartIcon className='btn'/>
+                    <ChatIcon className='btn' />
+                    <PaperAirplaneIcon className='btn'/>
+                </div>
+
+                <BookmarkIcon className='btn' />
+            </div>
+
+
+            {/* caption */}
+            <p className='p-5 truncate'>
+                <span className='font-bold mr-1'>UserName</span>
+                게시글
+            </p>
+        
+            {/* comments */}
+
+            {/* input box */}
+            <form className='flex items-center p-4'>
+                <EmojiHappyIcon className='h-7 '/>
+                <input 
+                    type="text" 
+                    placeholder="Add a comment..."
+                    className='border-none flex-1 focus:ring-0'/>
+                <button className='font-semibold text-blue-400 '>게시</button>
+            </form>
+        </div>
+    )
+}
+
+export default Post
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,7 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"),
+    require("tailwind-scrollbar"),
+    require("tailwind-scrollbar-hide")],
 }
